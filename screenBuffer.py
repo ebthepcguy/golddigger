ea0c73dea40ef9ce934df264af4dcb4f2291efe4
ebthepcguy <ebthepcguy@outlook.com>
2017-05-02 09:43:47 -0400

    Fixed Screen Flicker
    Used \033[F ANSI to clear screen and return to previous position.

---
+++ screenBuffer.py
@@ -0,0 +1,41 @@
+from engine.tile import Tile
+import os
+
+class ScreenBuffer(object):
+
+    def cls(n = 0):
+        if n == 0:
+            os.system('cls')
+        else:
+            print('\b'*n)
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.__buffer = [[ Tile(" ") for _ in range(width)] for _ in range(height)]
+
+    def clear(self):
+        self.__buffer = [[ Tile( " " ) for _ in range(self.__width)] for _ in range(self.__height)]
+
+    def addTile(self,tile, x, y):
+        if( x >= 0 and x < self.__width \
+        and y >= 0 and y < self.__height ):
+            self.__buffer[y][x] = tile
+
+    def draw(self):
+        printString = ""
+
+        for row in self.__buffer:
+            for tile in row:
+                printString += tile.char
+            printString += "\n"
+
+        # Stops extra newline from pushing screen up by one
+        printString = printString[:len(printString) - 2]
+
+        # Clears the screen WINDOWS ONLY
+        #os.system("CLS")
+        print("\033[F")
+        print(printString, end="")
+
+        self.clear()
